31c6ebda58e1564448a2e19f858de8da23afe0d6
jeon <wjadbwjd51@gamail.com>
2024-12-29 14:38:38 +0900

    성능 테스트를 위한 게시물 벌크 인서트 구현

---
+++ src/test/java/com/example/fastcampusmysql/domain/post/PostBulkInsertTest.java
@@ -0,0 +1,48 @@
+package com.example.fastcampusmysql.domain.post;
+
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import com.example.fastcampusmysql.utill.PostFixtureFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class PostBulkInsertTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    public void bulkinInsert() {
+        var easyRandom = PostFixtureFactory.get(
+                3L,
+                LocalDate.of(2022, 1, 1),
+                LocalDate.of(2022, 2, 1)
+        );
+
+        var stopWatch = new StopWatch();
+        stopWatch.start();
+
+        var posts = IntStream.range(0, 10000 * 100)
+                .parallel()
+                .mapToObj(i -> easyRandom.nextObject(Post.class))
+                .toList();
+
+
+        stopWatch.stop();
+        System.out.println("객체 생선 시간 : " + stopWatch.getTotalTimeSeconds());
+
+        var queryStopWatch = new StopWatch();
+        queryStopWatch.start();
+
+        postRepository.bulkInsert(posts);
+
+        queryStopWatch.stop();
+        System.out.println("DB 인서트 시간 : " + queryStopWatch.getTotalTimeSeconds());
+    }
+}
